e247d6adaad306d60d3822e0c2c9cf5a58daa82a
James Laird-Smith <jameslairdsmith@gmail.com>
2018-10-19 15:33:15 +0100

    Added selectors and maturity function.

---
+++ R/selectors.R
@@ -0,0 +1,15 @@
+#' @title Select properties of a contract
+#'
+#' @description This function allows users a quick shorthand for specifying the
+#'   properties of a contract. These do not need to be specified in prior steps.
+#'   Whether they are specified in future or prior steps, the values will only be replaced
+#'   once the contract is sheduled.
+#' @keywords date
+#' @export
+#'
+maturity <- function(x){x}
+
+
+#' @rdname maturity
+
+inception <- function(x){x}
