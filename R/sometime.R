e688b503c110282e51c8b5bd2ed5d60a2e28d24c
James Laird-Smith <jameslairdsmith@gmail.com>
2018-10-21 17:21:18 +0100

    Added sometime() functions.

---
+++ R/sometime.R
@@ -0,0 +1,14 @@
+#' @title Specify unknown date values in a contract
+#'
+#' @description This function acts as a stand-in for unknown date values in a contract.
+#' @param start_date the earliest possible date of the event.
+#' @param end_date the latest possible date of the event.
+#' @export
+
+sometime_between <- function(start_date, end_date){
+  NULL
+}
+
+sometime <- function(){
+  NULL
+}
