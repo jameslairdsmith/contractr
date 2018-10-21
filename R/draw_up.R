ccd65eec046e9038b92bad183a257c6b12211197
James Laird-Smith <jameslairdsmith@gmail.com>
2018-10-21 17:21:31 +0100

    Added draw up function.

---
+++ R/draw_up.R
@@ -0,0 +1,15 @@
+#' @title Fills in the missing parts of a contract
+#'
+#' @description This function takes a contract object and replaces all the unknown elements that it can.
+#' @param contract A contract object
+#' @export
+
+draw_up <- function(contract){
+  contract
+}
+
+
+# TO DO
+# Substitution of dates
+# Substitution of formulas
+# Splitting into event objects?
