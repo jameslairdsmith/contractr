4f091d0bec3ae1ca0b710884b413caf5a21854f5
James Laird-Smith <jameslairdsmith@gmail.com>
2018-10-21 17:20:40 +0100

    New add_maturity() function.

---
+++ R/add_maturity.R
@@ -0,0 +1,15 @@
+#' @title Add maturity date to a contract
+#'
+#' @param contract A contract object
+#' @param date The maturity date of the contract.
+#' @export
+
+add_maturity <- function(contract, date){
+  contract
+}
+
+
+# TO DO
+# Substitution of dates
+# Substitution of formulas
+# Splitting into event objects?
