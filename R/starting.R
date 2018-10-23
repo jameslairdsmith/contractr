6d5fd2f6e84a5a5a181df2f65933cf00acdb1f0a
James Laird-Smith <jameslairdsmith@gmail.com>
2018-10-23 17:52:13 +0100

    Created starting_from() function.

---
+++ R/starting.R
@@ -0,0 +1,44 @@
+#' @title Add a start date to a contract
+#'
+#' @description This function specifies the start date to a tidy contract.
+#' @param contract A contract to which the start date needs to be specified.
+#' @param date The start date
+#' @keywords contract, inception
+#' @export
+#' @examples
+#' library(magrittr)
+#' library(lubridate)
+#'
+#' make_contract() %>%
+#'   starting_from(dmy("01/01/2000")
+#'
+
+
+#starting <- function(contract, date){
+
+ # data <- as.Date(date)
+
+#  contract$terms <- contract$terms %>%  tibble::add_row(term_name = "maturity date", term_value = date)
+#  contract
+#}
+
+starting_from <- function(contract, start_date, id = "inception"){
+
+  add_clause(
+    contract,
+    starting_from_new(
+      start_date = start_date,
+      id = id
+    )
+  )
+}
+
+
+starting_from_new <- function(start_date, id) {
+    clause(
+      subclass = "starting",
+      start_date = start_date,
+      id = id
+    )
+  }
+
