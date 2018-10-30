ba3c598201aa34cc73d79f44845597d40222c03a
James Laird-Smith <jameslairdsmith@gmail.com>
2018-10-30 16:00:28 +0000

    changed arguments in function schedule().

--- R/schedule.R
+++ R/schedule.R
@@ -7,23 +7,23 @@
 schedule <- function(object, ...)
   UseMethod("schedule")
 
-#' Create the events from a contract
+#' Create the events of a contract
 #'
 #' For a contract with at least one term and no missing stipulations
 #' create the financial events that ought result from it.
-#' @param object A contract with at least one term.
-#' @return A tibble
+#' @param contract A contract with at least one term.
+#' @return A tibble housing the contract's events.
 #' @rdname schedule
 #' @importFrom tibble tibble
 #' @importFrom dplyr bind_rows
 #' @export
 
-schedule.contract <- function(object) {
+schedule.contract <- function(contract) {
 
 events <- tibble::tibble()
 
-for (i in seq(along = object$terms)){
-  new_events <- schedule(object$terms[[i]])
+for (i in seq(along = contract$terms)){
+  new_events <- schedule(contract$terms[[i]])
   events <- dplyr::bind_rows(new_events, events)
 }
 events
