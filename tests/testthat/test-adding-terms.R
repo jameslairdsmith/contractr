a0e7e90d65775a4a3202f3ddb10554bd2221fd51
James Laird-Smith <jameslairdsmith@gmail.com>
2018-10-31 18:32:13 +0000

    added tests.

--- tests/testthat/test-adding-terms.R
+++ tests/testthat/test-adding-terms.R
@@ -12,15 +12,46 @@ test_that("start details can be added to a contract", {
   expect_true(inherits(contract_a[["terms"]][[1]],"term_start"))
 })
 
-test_that("unique clauses can only be added to a contract once", {
+test_that("a different event name can be added to start term", {
+  events <-
+    contract() %>%
+    term_start(dmy("01/01/2018"), event_name = "purchase date") %>%
+    schedule()
+
+  expect_equal(events[1, "event_type"] %>% dplyr::pull(),"purchase date")
+})
+
+test_that("a receipt value can be specified for a start term", {
   contract_a <-
     contract() %>%
-    term_start(dmy("01/01/2018"))
+    term_start(as.Date("2000/02/01"), receipt = 1000)
 
-  expect_true(contract_a[["terms"]][[1]][["unique"]])
+  expect_equal(as.numeric(contract_a$stipulations[[2]]), 1000)
+
+  events <- contract_a %>% schedule()
+
+  expect_equal(events[1, "event_value"] %>% dplyr::pull(), 1000)
+})
+
+test_that("a payment value can be specified for a start term", {
+  contract_a <-
+    contract() %>%
+    term_start(as.Date("2000/02/01"), payment = 50)
+
+  expect_equal(as.numeric(contract_a$stipulations[[2]]), 50)
+
+  events <- contract_a %>% schedule()
+
+  expect_equal(events[1, "event_value"] %>% dplyr::pull(), -50)
+})
+
+test_that("only one of either payment or reciept can be specified for a start term", {
   expect_error(
     contract() %>%
-      term_start(dmy("01/01/2018")) %>%
-      term_start(dmy("01/01/2018"))
-  )
+      term_start(
+        as.Date("2000/02/01"),
+        payment = 50,
+        receipt = 10)
+    )
+
 })
--- tests/testthat/test-basic-functions.R
+++ tests/testthat/test-basic-functions.R
@@ -16,3 +16,16 @@ test_that("terms can be created", {
 
   expect_is(contract_a[["terms"]][[1]], "term")
 })
+
+test_that("unique clauses can only be added to a contract once", {
+  contract_a <-
+    contract() %>%
+    term_start(dmy("01/01/2018"))
+
+  expect_true(contract_a[["terms"]][[1]][["unique"]])
+  expect_error(
+    contract() %>%
+      term_start(dmy("01/01/2018")) %>%
+      term_start(dmy("01/01/2018"))
+  )
+})
