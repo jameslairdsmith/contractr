0d74d33e5ba242b7959d27e3f39b4da39b56916b
James Laird-Smith <jameslairdsmith@gmail.com>
2018-11-03 09:16:11 +0000

    updates to function make_date_seq().

--- R/make_date_seq.R
+++ R/make_date_seq.R
@@ -12,27 +12,62 @@
 #' @param include_last whether to include the last date
 #' @param replace_last whether to replace the last date with `end_date`
 #'
+#' @importFrom dplyr case_when
+#' @importFrom stringr str_to_lower
+#' @importFrom lubridate interval
+#'
 #' @export
 
 make_date_seq <- function(start_date,
                           end_date,
                           period_gap,
                           tolerance = 0.2,
-                          include_first = F,
+                          include_first = T,
                           include_last = T,
-                          replace_last = T){
+                          replace_last = F,
+                          allow_last_jump = F){
 
-  date_diff <- end_date - start_date
+  if(is.character(period_gap)){
+    period_gap <- stringr::str_to_lower(period_gap)
+  }
 
-  date_diff <- as.period(date_diff)
+  period_gap <-
+    dplyr::case_when(
+      period_gap == "quarterly" ~ months(3),
+      period_gap == "annually" ~ years(1),
+      period_gap == "monthly" ~ months(1),
+      period_gap == "weekly" ~ weeks(1),
+      period_gap == "daily" ~ days(1),
+      TRUE ~ as.period(period_gap)
+    )
 
-  n_periods <- suppressMessages(date_diff / period_gap)
+  if(inherits(period_gap, what = "Period") == FALSE){
+    stop("period_gap must be a `Period` object. See the lubridate package for details.")
+  }
 
-  rounded_n_periods <- floor(n_periods)
+  if(include_last == F & replace_last == T){
+    stop("replace_last is set to true, but last value already excluded as include_last == F")
+  }
 
-  print(n_periods - rounded_n_periods)
+  if(include_last == F & allow_last_jump == T){
+    stop("allow_last_jump is set to true, but last value already excluded as include_last == F")
+  }
 
-  if((n_periods - rounded_n_periods) > tolerance){
+  if(replace_last == T & allow_last_jump == T){
+    stop("allow_last_jump is set to true, but last value already being replaced as replace_last == T")
+  }
+
+  date_interval <- lubridate::interval(start = start_date, end = end_date)
+
+  n_periods <- date_interval / period_gap
+
+  if(allow_last_jump == T){
+    rounded_n_periods <- round(n_periods, digits = 0)
+  } else {
+    rounded_n_periods <- floor(n_periods)
+  }
+
+  if(abs(n_periods - rounded_n_periods) > tolerance){
     stop("Distance between supplied and projected last date is beyond tolerance level.")
   }
 
